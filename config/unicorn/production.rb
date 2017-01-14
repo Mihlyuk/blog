f13cfa786b62a19bdb612d18f570c752f7f30fd6
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2017-01-14 13:58:32 +0300

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,4 @@
-# set path to application
-app_dir = File.expand_path("../../..", __FILE__)
-
-working_directory app_dir
+working_directory '/var/www/blog'
 
 # Set unicorn options
 worker_processes 2
