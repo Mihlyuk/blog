0cebdd31c3d417ea3ecbd440257f563caf5a38c4
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2017-01-17 02:46:13 +0300

    task_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116204455) do
+ActiveRecord::Schema.define(version: 20170116234538) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170116204455) do
     t.integer  "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "task_id"
   end
 
   create_table "poems", force: :cascade do |t|
