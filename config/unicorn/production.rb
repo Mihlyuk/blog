713f975545435086e3fe925779fd45fee5cbfb5f
Konstantin Mikhliuk <mikhliuk.k@gmail.com>
2017-01-17 00:47:23 +0300

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,7 @@
-working_directory '/var/www/blog'
+# set path to application
+app_dir = File.expand_path("../../..", __FILE__)
+shared_dir = "#{app_dir}/../shared"
+working_directory app_dir
 
 # Set unicorn options
 worker_processes 2
@@ -6,11 +9,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen '/var/www/blog/shared/sockets/unicorn.sock', :backlog => 64
+listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
 
 # Logging
-stderr_path '/var/www/blog/log/unicorn.stderr.log'
-stdout_path '/var/www/blog/log/unicorn.stdout.log'
+stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+stdout_path "#{shared_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
-pid '/var/www/blog/pids/unicorn.pid'
+pid "#{shared_dir}/pids/unicorn.pid"
